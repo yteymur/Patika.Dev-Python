40479523e258ee9848cd4dc6737b41d570bd0e40
yteymur <84071833+yteymur@users.noreply.github.com>
2021-07-08 21:55:00 +0300

    Flatten and Reverse

---
+++ patika_dev--flatten & reverse.py
@@ -0,0 +1,39 @@
+def flatten(userinput=[[1,'a',['cat'],2],[[[3]],'dog'],4,5],result=[]):
+    for i in userinput:
+        if type(i) == list:
+            flatten(i,result)
+        else:
+            result.append(i)
+    return result
+
+
+
+def reverse(userinput=[[1, 2], [3, 4], [5, 6, 7]]):
+    for i in range(0,len(userinput)):
+        if type(userinput[i]) == list:
+            userinput[i]=userinput[i][::-1]
+            reverse(userinput[i])
+    return userinput[::-1]
+
+print("""-----------------------------------------------------------------------
+                  PATIKA.DEV PYTHON PROJECT
+        Actions;
+        
+        1-Flatten the list //// Default value= [[1,'a',['cat'],2],[[[3]],'dog'],4,5]
+        
+        2-Reverse a list ////  Default value= [[1, 2], [3, 4], [5, 6, 7]]
+        
+        Press "q" to exit.
+        
+      
+-----------------------------------------------------------------------
+      """)
+  
+while True:
+    action=(input("Select the action you want to do:"))
+    if action == "1" :
+        print(flatten())
+    if action == "2" :
+        print(reverse())
+    if action == "q" :
+        break
